ec75003c6f4762aa92bcb571001064f7c9f11491
ejazrahim <ejaz@codiea.io>
2024-03-04 16:12:55 +0500

    first commit2

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -1,2 +1,12 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+import Foundation
+
+public class MyPrintClass {
+    
+    public init() {}
+    
+    func printName(message: String){
+        print(message)
+    }
+}
