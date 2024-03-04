51b7fd3c2aba4fa8d0a0871dc63bb98169fab3e5
ejazrahim <ejaz@codiea.io>
2024-03-04 18:03:50 +0500

    s

--- Package.swift
+++ Package.swift
@@ -4,20 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "MyLibrary",
+    name: "LibEjazTest",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "MyLibrary",
-            targets: ["MyLibrary"]),
+            name: "LibEjazTest",
+            targets: ["LibEjazTest"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "MyLibrary"),
+            name: "LibEjazTest"),
         .testTarget(
-            name: "MyLibraryTests",
-            dependencies: ["MyLibrary"]),
+            name: "LibEjazTestTests",
+            dependencies: ["LibEjazTest"]),
     ]
 )
